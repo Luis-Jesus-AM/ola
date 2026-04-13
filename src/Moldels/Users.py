f7af9473875979dd7ac8b5e144cf987085994f7e
Jesusito <23308060610060@cetis61.edu.mx>
2026-04-13 10:58:00 -0500

    Update Users.py
    holi

--- src/Moldels/Users.py
+++ src/Moldels/Users.py
@@ -1,5 +1,5 @@
 import bcrypt
-from .database import Database 
+from .database import Database
 
 class UsuarioModel:
     def __init__(self):
@@ -8,6 +8,31 @@ class UsuarioModel:
     def registrar(self, usuario_data):
         # encriptar contraseña 
         salt = bcrypt.gensalt()
-        hashed_pw = bcrypt.hashpw(usuario-data.password.encode('utf-8'),salt)
+        hashed_pw = bcrypt.hashpw(usuario_data.password.encode('utf-8'),salt)
         
+        conn =self.db.get_connection()
+        cursor = conn.cursor()
+        try:
+            cursor.execute(
+                "INSERT INTO usuario (nombre, email, password) VALUES (%s, %s, %s)",
+                (usuario_data.nombre, usuario_data.email, hashed_pw.decode('utf-8'))
+            )
+            conn.commit()
+            return True
+        except Exception as e: 
+            print(f"Error: {e}")
+            return False 
+        finally:
+            conn.close()
+            
+    def validar_login(self, email, password):
+            conn = self.db.get_connection()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELEC = FROM usuario WHERE email=%s", (email))
+            user = cursor.fetchone()
+            conn.close()
+            
+            if user and bcrypt.checkpw(password.encode('utf-8'), user['password'].encode('utf-8')):
+                return user 
+            return None         
         
